a32eff96f819b1c8355616f69b2de7936ed8d566
DapP256 <danielandres.perezperez@alumnos.uai.edu.ar>
2026-09-18 23:31:35 -0300

    Initial commit - FirmaYA backend

---
+++ src/main/java/com/firmaya/api/FirmayaApiApplication.java
@@ -0,0 +1,13 @@
+package com.firmaya.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirmayaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirmayaApiApplication.class, args);
+	}
+
+}
